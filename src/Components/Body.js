65833d38f03a4db5bf2c958515bae3776b32bb13
Gavaskar <gavaskaraeg@gmail.com>
2023-10-11 08:44:14 +0000

    started designing body part

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,32 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Body() {
   return (
-    <div class="card-group">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <MainDiv>
+      <div >
+        <p className='text-center fs-4 fw-bolder'>Why We?</p>
+      </div>
+      <div class="card mb-3" style={{maxWidth: "540px"}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="..."/>
     </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Safety and Security</h5>
+        <p class="card-text">Your safety is our top priority. Our cab service is equipped with the latest security features and stringent safety measures to ensure you have a worry-free journey</p>
+      </div>
     </div>
   </div>
 </div>
+ 
+</MainDiv>
   )
 }
+
+const MainDiv = styled.div`
+height:90vh;
+margin-top:10%;
+`
